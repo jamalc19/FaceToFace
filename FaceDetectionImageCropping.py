2bfee8b1b10c90138bcc31142f2a82e202fea38a
michaelzcjia <michael.zc.jia@gmail.com>
2019-03-15 15:11:45 -0400

    Temporary

--- FaceDetectionImageCropping.py
+++ FaceDetectionImageCropping.py
@@ -3,9 +3,8 @@ import numpy as np
 from autocrop import autocrop as ac
 from shutil import copy
 
-path = "./Parsed/"
 
-def crop(path = "./Parsed/", dest = "./Cropped/", pixel = 256):
+def crop(path="./Parsed3_15/", dest="./Cropped128/", pixel=128):
     """
     Crop all of the images in each of the folders in the root directory
     and insert those images into a folder named "crop" in each image folder.
@@ -15,6 +14,10 @@ def crop(path = "./Parsed/", dest = "./Cropped/", pixel = 256):
 
     returns None
     """
+    reject_dir ="./reject"
+    # Make a reject folder
+    if os.path.isdir(reject_dir) == False:
+        os.mkdir(reject_dir)
 
     # For every person
     for p_id in os.listdir(path):
@@ -35,7 +38,31 @@ def crop(path = "./Parsed/", dest = "./Cropped/", pixel = 256):
             reject_d=err_dir,
             fheight=pixel,
             fwidth=pixel,
-            facePercent=85)
+            facePercent=90)
+
+
+def organize(path="./Cropped128/", dest="./OrganizedImages/"):
+    '''
+    Organizes cropped images into emotion folders.
+
+    returns None
+    '''
+
+    # For every person in the root folder
+    for p_id in os.listdir(path):
+        in_dir = path + p_id
+        # For every emotion in a person folder
+        for img in os.listdir(in_dir):
+            emo = img.split("_")[0]
+            new_dir = dest + "\\" + emo
+            # Make an emotion folder if it doesn't exist
+            if os.path.isdir(new_dir) == False:
+                os.mkdir(new_dir)
+            # Copy the image to the new destination
+            img_path = in_dir + "\\" + img
+            copy(img_path, new_dir)
+
 
 if __name__ == "__main__":
     crop()
+    # organize()
